e32280ca9e284108c5ef84bfd6fbbcbf88be4c47
aljariri <issaaljariri0@gmail.com>
2026-09-22 00:50:02 +0300

    solved questions from Q1 to Q9

---
+++ q6.js
@@ -0,0 +1,36 @@
+const prompt = require("prompt-sync")();
+
+let n = Number(prompt("Enter a number: "));
+
+switch (n) {
+  case 1:
+    console.log("One");
+    break;
+  case 2:
+    console.log("Two");
+    break;
+  case 3:
+    console.log("Three");
+    break;
+  case 4:
+    console.log("Four");
+    break;
+  case 5:
+    console.log("Five");
+    break;
+  case 6:
+    console.log("Six");
+    break;
+  case 7:
+    console.log("Seven");
+    break;
+  case 8:
+    console.log("Eight");
+    break;
+  case 9:
+    console.log("Nine");
+    break;
+
+  default:
+    console.log("Please try again");
+}
